430039973f72ef267a17bd47d6d4801bb831f7f8
Yokota <ryok90@gmail.com>
2019-10-05 18:54:06 -0300

    ignore

--- public/index.php
+++ public/index.php
@@ -7,9 +7,6 @@ use Zend\Stdlib\ArrayUtils;
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 chdir(dirname(__DIR__));
 
